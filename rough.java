b21c2efcefede2d37feb022ef3a2fa3fe7ef2e99
Prachi Singh <161819710+01prachisingh@users.noreply.github.com>
2026-07-16 21:13:23 +0530

    Add files via upload

---
+++ rough.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class rough {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+
+        // int age = sc.nextInt();
+        // if(age>18){
+        //     System.out.println("Adult");
+        // }
+        // else{
+        //     System.out.println("Not adult");
+        // }
+
+
+        // int n = sc.nextInt();
+        // if(n%2==0){
+        //     System.out.println("even");
+        // }
+        // else{
+        //     System.out.println("odd");
+        // }
+
+
+        //    int a = sc.nextInt();
+        //    int b = sc.nextInt();
+        // if(a==b){
+        //     System.out.println("equal");
+        // }
+        // else if(a>b){
+        //         System.out.println("greater");
+        //     }
+        //     else{
+        //     System.out.println("smaller");
+        //     }
+
+        //switch
+        // int button = sc.nextInt();
+        // if(button ==1){
+        //     System.out.println("hello");
+        // }
+        // else if(button == 2){
+        //     System.out.println("namaste");
+        // }
+        // else if(button == 3){
+        //     System.out.println("bonjour");
+        // }
+        // else{
+        // System.out.println("invalid");
+        // }
+        int button = sc.nextInt();
+        switch (button) {
+            case 1: System.out.println("hello");
+                break;
+            case 2: System.out.println("namaste");
+                break;
+            default:
+                break;
+        }
+        }
+    }
+    
+    
